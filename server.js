8415a486ccef2637c72b50d627975576af218a1e
jevans-ui <jevans@townofpageland.com>
2026-05-02 01:16:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.options('*', cors());
 app.use(express.json());
 app.use(express.static('.'));
 
